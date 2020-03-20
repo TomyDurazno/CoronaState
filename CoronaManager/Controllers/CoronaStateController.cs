38a8343fe19fb740c493966d6b45b55c09eb11d8
tcordara <tcordara@virtualmind.io>
2020-03-20 00:52:30 -0300

    Add project files.

---
+++ CoronaManager/Controllers/CoronaStateController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace CoronaManager.Controllers
+{
+    [ApiController]
+    [Route("/countries")]
+    public class CoronaStateController : ControllerBase
+    {
+        string Url => "https://corona.lmao.ninja/countries";
+
+        private readonly ILogger<CoronaStateController> _logger;
+
+        public CoronaStateController(ILogger<CoronaStateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CoronaState>> Get()
+        {
+            var client = new RestClient(new Uri(Url));
+
+            var request = new RestRequest(Method.GET);
+
+            var result = await client.ExecuteTaskAsync(request);
+
+            return JArray.Parse(result.Content).ToObject<List<CoronaState>>();
+        }
+    }
+}
---
+++ CoronaManager/Models/CoronaState.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CoronaManager
+{
+    public class CoronaState
+    {
+        public string country { get; set; }
+        public int cases { get; set; }
+        public int todayCases { get; set; }
+        public int deaths { get; set; }
+        public int todayDeaths { get; set; }
+        public int recovered { get; set; }
+        public int active { get; set; }
+        public int critical { get; set; }
+        public int casesPerOneMillion { get; set; }
+    }
+}
